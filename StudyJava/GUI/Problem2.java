7a424012835b19de3935134b9b5563b8a8bfaa16
DunDun <eheh12321@gmail.com>
2021-12-07 11:59:11 +0900

    JAVA Swing 계산기 - 키보드로 숫자입력 가능하게 키보드 리스너 추가

--- StudyJava/GUI/Problem2.java
+++ StudyJava/GUI/Problem2.java
@@ -2,8 +2,7 @@ package CodingTestMemory.StudyJava.GUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 class Problem2 {
 
@@ -18,8 +17,9 @@ class Problem2 {
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setTitle("Calculator");
             setLayout(new BorderLayout());
+            setFocusable(true);
 
-            /**
+            /*
              * 계산기 계산결과 라벨
              */
             label_ans = new JLabel("0");
@@ -27,14 +27,14 @@ class Problem2 {
             label_ans.setHorizontalAlignment(SwingConstants.RIGHT);
             this.add(label_ans, BorderLayout.NORTH);
 
-            /**
+            /*
              * 계산기 버튼 레이아웃 패널
              */
             JPanel p = new JPanel(new GridLayout(4, 4, 3, 3));
             this.add(p, BorderLayout.CENTER);
 
-            /**
-             * 숫자 버튼 구현
+            /*
+              숫자 버튼 구현
              */
             for(int i = 0; i <= 9; i++) {
                 numBtn[i] = new JButton(Integer.toString(i));
@@ -56,7 +56,7 @@ class Problem2 {
                 });
             }
 
-            /**
+            /*
              * 연산자 버튼 구현
              */
             String[] funcStr = new String[] {"+", "-", "*", "AC", "=", "/"};
@@ -107,16 +107,28 @@ class Problem2 {
                                 }
                                 break;
                         }
+                        MyGUI.this.requestFocus(true); // 포커스 반환
                     }
                 });
             }
 
-            /**
+            /*
              * 키보드 이벤트 리스너
              */
+            this.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    System.out.println("키 입력 발생");
+                    int key = e.getKeyCode();
+                    if(key >= KeyEvent.VK_0 && key <= KeyEvent.VK_9) {
+                        numBtn[key - 48].doClick();
 
+                    }
+                    validate();
+                }
+            });
 
-            /**
+            /*
              * 레이아웃에 버튼 배치
              */
             for(int j = 0, cnt = 7; j < 3; j++) {
@@ -130,7 +142,6 @@ class Problem2 {
             for(int i = 3; i < 6; i++) {
                 p.add(funcBtn[i]);
             }
-
             setSize(300, 450);
             setVisible(true);
         }
