31992d11a7950527de509de43555a29ac999a799
eheh12321 <eheh12321@gmail.com>
2022-10-10 16:01:36 +0900

    BST 코드 수정 - main 메서드 따로 만들어둔거 지저분해보여서 그냥 뺌 - 재귀를 사용하지 않는 BST 코드로 수정

--- 자료구조/Tree/BinarySearchTree.java
+++ 자료구조/Tree/BinarySearchTree.java
@@ -1,10 +1,15 @@
 package CodingTestMemory.자료구조.Tree;
 
-import java.util.Scanner;
+import java.util.Stack;
 
+/**
+ * BST
+ * 코드 참고
+ * - https://yaboong.github.io/data-structures/2018/02/12/binary-search-tree/
+ * - https://github.com/eugenp/tutorials/blob/master/data-structures/src/main/java/com/baeldung/tree/BinaryTree.java
+ */
 public class BinarySearchTree {
-
-    public static class Node {
+    public class Node {
         Node left;
         Node right;
         int key;
@@ -14,111 +19,39 @@ public class BinarySearchTree {
         }
     }
 
-    public static Node root;
+    private static Node root;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("===================");
-            System.out.println("BST Tree");
-            System.out.println("1: 탐색");
-            System.out.println("2: 삽입");
-            System.out.println("3: 삭제");
-            System.out.println("4: 순회");
-            System.out.println("0: 종료");
-            System.out.println("===================");
-            System.out.print(">> 명령어를 입력해주세요: ");
-            int op = sc.nextInt();
-            switch (op) {
-                case 0:
-                    System.out.println("시스템을 종료합니다");
-                    return;
-                case 1:
-                    System.out.print(">> 탐색할 키값을 입력해주세요: ");
-                    findNode(sc.nextInt());
-                    break;
-                case 2:
-                    System.out.print(">> 삽입할 키값을 입력해주세요: ");
-                    addNode(sc.nextInt());
-                    break;
-                case 3:
-                    System.out.print(">> 삭제할 키값을 입력해주세요: ");
-                    deleteNode(sc.nextInt());
-                    break;
-                case 4:
-                    System.out.print(">> 순회 방법을 입력하세요 (1=preorder, 2=inorder, 3=postorder): ");
-                    switch (sc.nextInt()) {
-                        case 1:
-                            System.out.println("[전위 순회]");
-                            System.out.print("[ ");
-                            preorder(root);
-                            System.out.println("]");
-                            break;
-                        case 2:
-                            System.out.println("[중위 순회]");
-                            System.out.print("[ ");
-                            inorder(root);
-                            System.out.println("]");
-                            break;
-                        case 3:
-                            System.out.println("[후위 순회]");
-                            System.out.print("[ ");
-                            postorder(root);
-                            System.out.println("]");
-                            break;
-                    }
-                    break;
-                default:
-                    System.out.println("잘못된 명령어입니다");
-                    break;
+    public Node find(int key) {
+        Node current = root;
+        while (current != null) {
+            if (current.key == key) {
+                break;
             }
+            current = current.key < key ? current.right : current.left;
         }
+        return current;
     }
 
-    public static Node findNode(int key) {
-        if (root == null) {
-            System.out.println(">> 트리가 비어있습니다!");
-            return null;
-        }
-        Node focusNode = root;
-        int height = 0;
-        while (focusNode.key != key) {
-            if (focusNode.key < key) focusNode = focusNode.right;
-            else focusNode = focusNode.left;
-            height++;
-            if (focusNode == null) {
-                System.out.println(">> 해당 노드가 존재하지 않습니다 :P");
-                return null;
-            }
-        }
-        System.out.printf(">> 값이 [%d] 높이에 존재합니다!\n", height);
-        return focusNode;
-    }
-
-    public static void addNode(int key) {
+    public void insert(int key) {
         Node newNode = new Node(key);
         if (root == null) {
             root = newNode;
-            System.out.printf(">> [%d] 노드를 트리에 삽입했습니다\n", key);
         } else {
             Node parent;
-            Node focusNode = root;
+            Node current = root;
             while (true) {
-                parent = focusNode;
-                if (focusNode.key == key) {
-                    System.out.println(">> 이미 값이 존재합니다!");
-                    return;
-                } else if (key < focusNode.key) {
-                    focusNode = parent.left;
-                    if (focusNode == null) { // 단말노드이면 삽입
-                        System.out.printf(">> [%d] 노드를 트리에 삽입했습니다\n", key);
+                parent = current;
+                if (current.key == key) {
+                    throw new RuntimeException("이미 해당 노드가 존재하고 있습니다.");
+                } else if (key < current.key) {
+                    current = parent.left;
+                    if (current == null) { // 단말노드이면 삽입
                         parent.left = newNode;
                         return;
                     }
                 } else {
-                    focusNode = parent.right;
-                    if (focusNode == null) { // 단말노드이면 삽입
-                        System.out.printf(">> [%d] 노드를 트리에 삽입했습니다\n", key);
+                    current = parent.right;
+                    if (current == null) { // 단말노드이면 삽입
                         parent.right = newNode;
                         return;
                     }
@@ -127,90 +60,143 @@ public class BinarySearchTree {
         }
     }
 
-    public static void deleteNode(int key) {
+    public void delete(int key) {
         if (root == null) {
-            System.out.println(">> 트리가 비어있습니다!");
             return;
         }
         Node parent = root; // 삭제될 노드의 부모 노드
-        Node focusNode = root; // 삭제될 노드
-        while (focusNode.key != key) {
-            parent = focusNode;
-            if (key < focusNode.key) focusNode = parent.left;
-            else focusNode = parent.right;
+        Node current = root; // 삭제될 노드
 
-            if (focusNode == null) {
-                System.out.println(">> 해당 노드가 없습니다!");
-                return;
+        while (current.key != key) { // 삭제될 노드를 먼저 탐색하는 과정
+            parent = current;
+            if (key < current.key) {
+                current = parent.left;
+            } else {
+                current = parent.right;
+            }
+
+            if (current == null) {
+                throw new RuntimeException("해당 노드를 찾을 수 없습니다.");
             }
         }
+
         Node replaceNode;
-        // 단말 노드인 경우
-        if (focusNode.left == null && focusNode.right == null) {
-            if (focusNode == root) root = null;
-            else if (parent.left != null) parent.left = null;
-            else parent.right = null;
-        }
-        // 왼쪽 서브트리만 있는 경우
-        else if (focusNode.right == null) {
-            replaceNode = focusNode.left;
-            if (focusNode == root) root = replaceNode;
-            else if (parent.left != null) parent.left = replaceNode;
-            else parent.right = replaceNode;
-        }
-        // 오른쪽 서브트리만 있는 경우
-        else if (focusNode.left == null) {
-            replaceNode = focusNode.right;
-            if (focusNode == root) root = replaceNode;
-            else if (parent.left != null) parent.left = replaceNode;
-            else parent.right = replaceNode;
-        }
-        // 양쪽 서브트리가 모두 있는 경우
-        else {
-            Node rightSubTree = focusNode.right;
-            replaceNode = getRightMinNode(focusNode.right); // 오른쪽 서브트리 안에서 최소 노드
+        // Case 1. 단말 노드인 경우
+        if (current.left == null && current.right == null) {
+            if (current == root) root = null; // Root 노드를 삭제하는 경우
+            else if (parent.left != null) parent.left = null; // 왼쪽 자식 노드를 삭제하는 경우
+            else parent.right = null; // 오른쪽 자식 노드를 삭제하는 경우
 
-            if (focusNode == root) root = replaceNode;
-            else if (parent.left != null) parent.left = replaceNode;
-            else parent.right = replaceNode;
-
-            replaceNode.right = rightSubTree;
-            if (replaceNode == rightSubTree) replaceNode.right = null; // 우측 서브트리에 노드가 하나밖에 없는 경우
-            replaceNode.left = focusNode.left;
+            return;
         }
-        System.out.printf(">> [%d] 노드를 트리에서 삭제했습니다\n", key);
+
+        // Case 2. 왼쪽 서브트리만 있는 경우
+        if (current.right == null) {
+            replaceNode = current.left;
+            if (current == root) root = replaceNode; // Root 노드를 삭제하는 경우
+            else if (parent.left != null) parent.left = replaceNode; // 왼쪽 자식 노드를 삭제하는 경우
+            else parent.right = replaceNode; // 오른쪽 자식 노드를 삭제하는 경우
+
+            return;
+        }
+
+        // Case 3. 오른쪽 서브트리만 있는 경우
+        if (current.left == null) {
+            replaceNode = current.right;
+            if (current == root) root = replaceNode; // Root 노드를 삭제하는 경우
+            else if (parent.left != null) parent.left = replaceNode; // 왼쪽 자식 노드를 삭제하는 경우
+            else parent.right = replaceNode; // 오른쪽 자식 노드를 삭제하는 경우
+
+            return;
+        }
+
+        // Case 4. 양쪽 서브트리가 모두 있는 경우
+        Node rightSubTree = current.right;
+        replaceNode = getRightMinNode(current.right); // 오른쪽 서브트리 안에서 최소 노드
+
+        // replaceNode를 Current Node 자리로 대체
+        if (current == root) root = replaceNode; // Root 노드를 삭제하는 경우
+        else if (parent.left != null) parent.left = replaceNode; // 왼쪽 자식 노드를 삭제하는 경우
+        else parent.right = replaceNode; // 오른쪽 자식 노드를 삭제하는 경우
+
+        replaceNode.right = current.right; // 나머지 서브트리를 오른쪽 자식에 이어붙임
+        if (replaceNode == rightSubTree) replaceNode.right = null; // 우측 서브트리의 루트 노드가 가장 최솟값이었던 경우
+        replaceNode.left = current.left; // 대체 노드의 왼쪽 자식은 기존에 있던 왼쪽 서브트리를 이어붙임
     }
 
-    private static Node getRightMinNode(Node rightChildRootNode) {
-        Node parent = rightChildRootNode;
+    // 오른쪽 서브트리에서 가장 작은 노드를 리턴하고, 서브트리에서 해당 노드를 null 처리 하는 것 까지 같이 진행
+    // 단, 우측 서브트리에서 루트 노드가 가장 최소 노드인 경우는 null 처리가 되지 않으므로 바깥에서 체크해줘야함
+    private Node getRightMinNode(Node rightChildRootNode) {
         Node focusNode = rightChildRootNode;
-
+        Node parent = rightChildRootNode;
         while (focusNode.left != null) {
             parent = focusNode;
             focusNode = focusNode.left;
         }
-        parent.left = null;
+        parent.left = focusNode.right;
         return focusNode;
     }
 
-    public static void preorder(Node node) {
-        if (node == null) return;
-        System.out.print(node.key + " ");
-        preorder(node.left);
-        preorder(node.right);
+    /**
+     * (심화 중 심화) 재귀 대신 스택을 사용하는 트리 순회 방법
+     */
+
+    public void preorder() {
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+        stack.push(root);
+
+        while (current != null && !stack.isEmpty()) {
+            current = stack.pop();
+            System.out.println(" " + current.key);
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
     }
 
-    public static void inorder(Node node) {
-        if (node == null) return;
-        inorder(node.left);
-        System.out.print(node.key + " ");
-        inorder(node.right);
+    public void inorder() {
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) { // 왼쪽으로 끝까지 진행
+                stack.push(current);
+                current = current.left;
+            }
+            Node top = stack.pop();
+            System.out.print(" " + top.key); // 방문
+            current = top.right; // 오른쪽으로 이동
+        }
     }
 
-    public static void postorder(Node node) {
-        if (node == null) return;
-        postorder(node.left);
-        postorder(node.right);
-        System.out.print(node.key + " ");
+    public void postorder() {
+        Stack<Node> stack = new Stack<>();
+        Node prev = root;
+        Node current = root;
+        stack.push(root);
+
+        while (current != null || !stack.isEmpty()) {
+            current = stack.peek();
+            boolean hasChild = (current.left != null || current.right != null);
+            boolean isPrevLastChild = (prev == current.right || (prev == current.left && current.right == null));
+
+            if (!hasChild || isPrevLastChild) {
+                current = stack.pop();
+                System.out.println(" " + current.key);
+                prev = current;
+            } else {
+                if (current.right != null) {
+                    stack.push(current.right);
+                }
+                if (current.left != null) {
+                    stack.push(current.left);
+                }
+            }
+        }
     }
 }
