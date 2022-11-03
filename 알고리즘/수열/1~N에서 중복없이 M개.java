160ba583be9e7fa03fe7c9d6f5cfb31fd30265d9
이도형 <eheh12321@gmail.com>
2022-11-03 10:49:15 +0900

    Update 1~N에서 중복없이 M개.java

--- 알고리즘/수열/1~N에서 중복없이 M개.java
+++ 알고리즘/수열/1~N에서 중복없이 M개.java
@@ -3,6 +3,15 @@
 
 import java.util.*;
 
+/**
+ * N = 3 / M = 3
+ * [1, 2, 3]
+ * [1, 3, 2]
+ * [2, 1, 3]
+ * [2, 3, 1]
+ * [3, 1, 2]
+ * [3, 2, 1]
+ */
 public class Solution {
 
     private boolean[] visited;
